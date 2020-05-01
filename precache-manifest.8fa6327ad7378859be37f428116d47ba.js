e44dba643011f001ee53e5426291e779d2718bb0
mohamedmeska-aws <mohamedmostafa@awstreams.com>
2020-05-01 15:00:27 +0200

    Updates

---
+++ precache-manifest.8fa6327ad7378859be37f428116d47ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5fba73647d11a90e8636626c35628d",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "e82e8f440ffbe1c3b668",
+    "url": "/todo/static/js/2.c61afb27.chunk.js"
+  },
+  {
+    "revision": "81d6b8754462766ac20934c40cd03329",
+    "url": "/todo/static/js/2.c61afb27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "474189d3758decfc033c",
+    "url": "/todo/static/js/main.e9392788.chunk.js"
+  },
+  {
+    "revision": "54a17aca2412126c5033",
+    "url": "/todo/static/js/runtime-main.67d96b9e.js"
+  }
+]);
